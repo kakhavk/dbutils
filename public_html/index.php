8177de3240265341b999bc4677b491d90e758a5e
Gogs <gogs@fake.local>
2015-12-16 18:17:25 +0400

    hi

--- public_html/index.php
+++ public_html/index.php
@@ -1,11 +1,6 @@
 <?php
 echo '<div style="margin-top:20px; margin-left:10px;"><ul>';
-$exclude=array('.','..','index.php','LICENSE', 'conf.php');
-if($dir=opendir('./')){
-	while(false!==($item=readdir($dir))){
-		if(!in_array($item, $exclude) && is_file($item)) echo '<li><a href="'.$item.'">'.$item.'</li>';
-	}
-	closedir($dir);
-}
+echo '<li><a href="example.php">Example</li>';
+echo '<li><a href="test.php">Test</li>';
 echo '</ul><div>';
 ?>
