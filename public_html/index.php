0a9d466edb1752f975e1738051a74f313cb81946
Gogs <gogs@fake.local>
2015-12-16 23:22:21 +0400

    hiall

--- public_html/index.php
+++ public_html/index.php
@@ -2,8 +2,4 @@
 require_once 'header.php';
 
 require_once 'footer.php';
-echo '<div style="margin-top:20px; margin-left:10px;"><ul>';
-echo '<li><a href="example.php">Example</li>';
-echo '<li><a href="test.php">Test</li>';
-echo '</ul><div>';
 ?>
