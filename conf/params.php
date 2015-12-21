63876ccce51653b4cec018ba04041e65a346142a
Kakhaber Kashmadze <kakhavk@gmail.com>
2015-12-21 16:30:00 +0400

    Update params.php

--- conf/params.php
+++ conf/params.php
@@ -1,13 +1,7 @@
 <?php
-/*
-define('DBHOST','192.168.1.245');
-define('DBUSER','sa');
-define('DBPASS','1231');
-define('DBNAME','test');
-*/
 define('DBHOST','localhost');
-define('DBUSER','hi');
-define('DBPASS','hiall');
-define('DBNAME','test');
+define('DBUSER','username');
+define('DBPASS','password');
+define('DBNAME','databasename');
 
 ?>
