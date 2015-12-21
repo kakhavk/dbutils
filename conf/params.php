1cbe12d03bacf5dff7286a805d626a76054c52df
Gogs <gogs@fake.local>
2015-12-21 16:30:57 +0400

    gitignore

--- conf/params.php
+++ conf/params.php
@@ -1,10 +1,4 @@
 <?php
-/*
-define('DBHOST','192.168.1.245');
-define('DBUSER','sa');
-define('DBPASS','1231');
-define('DBNAME','test');
-*/
 define('DBHOST','localhost');
 define('DBUSER','hi');
 define('DBPASS','hiall');
